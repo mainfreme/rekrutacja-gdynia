795ca9261051a0714fd0fcf446fb1803b78a57de
Soft Well House <bialonj@gmail.com>
2026-04-09 20:52:12 +0200

    endpoint for upload file

--- backend/app/Http/Controllers/Api/UploadImportController.php
+++ backend/app/Http/Controllers/Api/UploadImportController.php
@@ -1,15 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\ImportFileRequest;
+use App\Jobs\ProcessImportJob;
+use App\Models\Import;
+use Illuminate\Http\JsonResponse;
+use App\Http\Resources\ImportResource;
+use Throwable;
+use Symfony\Component\HttpFoundation\Response;
 
-class UploadImportController extends Controller
+class UploadImportController extends ApiController
 {
-    public function upload(ImportFileRequest $request)
+    public function upload(ImportFileRequest $request): JsonResponse
     {
-        return response()->json(['message' => 'Import uploaded successfully']);
+        $file = $request->file('file');
+
+        try {
+            $import = Import::create([
+                'file_name' => $file->getClientOriginalName(),
+            ]);
+
+            ProcessImportJob::dispatch($import->id, $file);
+
+            $import->refresh();
+
+            return $this->successResponse([
+                'message' => 'Import uploaded successfully',
+                'import' => ImportResource::make($import),
+            ]);
+        } catch (Throwable $e) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
